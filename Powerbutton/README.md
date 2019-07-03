373c8448331a49f5c82f2888ddafd55416b5f8b6
Rezwin Rafeek <rezraf77@gmail.com>
2019-07-03 08:34:23 +0530

    comitting from giteasy

--- Powerbutton/README.md
+++ Powerbutton/README.md
@@ -1,4 +1,4 @@
 # Adding a Shutdown Button to Rpi
 
-##AIM
+## AIM
 to add a button to rpi that can be used to poweroff the rpi.This is done by connecting the rpi to an arduino via a USB cable and then using the serial interface to send a message to py telling it to shutdown everytime the arduino senses a button press 
---
+++ giteasy.py
@@ -0,0 +1,19 @@
+from os import system
+
+print("GitEasy ver 1.0")
+while True:
+    command = input(">> ")                  #change this to what you want your prompt to look like
+
+    if command == "commit" :
+        mesg = input("Enter message :")
+        system("git add -A")
+        command2 = "git commit -m \" " + mesg + "\""
+        system(command2)
+
+    elif command == "push":
+        system("git push")
+    
+    else:
+        system(command)
+
+#End of While
